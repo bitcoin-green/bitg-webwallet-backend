e59ea8eb24c218d4c68dc7f4dd048a7e050633ab
Ohta Yoshida <ohtayoshida8912@yahoo.com>
2018-12-19 08:12:38 +1000

    update string parser

--- modules/utils.js
+++ modules/utils.js
@@ -18,6 +18,7 @@ exports.promisify = function (fn, args) {
 };
 
 exports.stringToBoolean = function (string) {
+  if (string === undefined || string === null) return false;
   switch (String(string).toLowerCase().trim()) {
     case 'true': case 'yes': case '1': return true;
     case 'false': case 'no': case '0': case null: return false;
