8b701277fce88a9488a4d7f33ebc2e03a0d06e0b
Ohta Yoshida <ohtayoshida8912@yahoo.com>
2018-12-19 06:47:30 +1000

    add debug log

--- controllers/rpc.js
+++ controllers/rpc.js
@@ -1,16 +1,19 @@
+const { toBoolean } = require('../modules/utils');
 const config = require('../config');
 const client = config.rpcClient;
 
 exports.sendrawtransaction = (req, res) => {
   const raw = req.body.raw;
-  const allowhighfees = Boolean(req.body.allowhighfees);
-  const swifttx = Boolean(req.body.swifttx);
+  const allowhighfees = toBoolean(req.body.allowhighfees);
+  const swifttx = toBoolean(req.body.swifttx);
 
+  console.log('============ raw, allowhighfees, swifttx ============', raw, allowhighfees, swifttx);
   if (!raw) return res.status(400).send({ result: 'error', message: 'Empty raw data!' });
 
   try {
     return client.call('sendrawtransaction', [raw, allowhighfees, swifttx], (err, result) => {
-      if (err) return res.status(400).send({ result: 'error', message: 'Error occured while sending raw transaction', error: err });
+      console.log('============ err, result ============', err, result);
+      if (err) return res.status(400).send({ result: 'error', message: 'Error occured while sending raw transaction', error: err.error });
       return res.status(200).send({ result: 'ok', data: result });
     });
   } catch (error) {
--- modules/utils.js
+++ modules/utils.js
@@ -16,3 +16,11 @@ exports.promisify = function (fn, args) {
     }
   });
 };
+
+exports.toBoolean = function (string) {
+  switch (string.toLowerCase().trim()) {
+    case 'true': case 'yes': case '1': return true;
+    case 'false': case 'no': case '0': case null: return false;
+    default: return Boolean(string);
+  }
+};
