6296c5d79ad96bf25980bf624faeb270bd212049
konez2k <konez@protonmail.com>
2019-06-20 01:47:18 +0200

    implement balance and unspent rpc calls

--- controllers/rpc.js
+++ controllers/rpc.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const { stringToBoolean } = require('../modules/utils');
 const config = require('../config');
 const client = config.rpcClient;
@@ -17,4 +19,30 @@ exports.sendrawtransaction = (req, res) => {
   } catch (error) {
     return res.status(400).send({ result: 'error', message: 'Rpc call error', error });
   }
+};
+
+exports.getbalance = (req, res) => {
+  if (!req.query.address) return res.status(404).send({ result: 'error', message: 'invalid address' });
+
+  return axios({
+    method: 'get',
+    url: 'https://www.coinexplorer.net/api/v1/BITG/address/balance?address=' + req.query.address,
+  }).then(response => {
+    res.status(200).send({ result: 'ok', data: response.data.result });
+  }).catch(error => {
+    res.status(500).send({ result: 'error', message: 'internal error', error });
+  });
+};
+
+exports.getunspent = (req, res) => {
+  if (!req.query.address) return res.status(404).send({ result: 'error', message: 'invalid address' });
+
+  return axios({
+    method: 'get',
+    url: 'https://www.coinexplorer.net/api/v1/BITG/address/unspent?address=' + req.query.address,
+  }).then(response => {
+    res.status(200).send({ result: 'ok', data: response.data.result });
+  }).catch(error => {
+    res.status(500).send({ result: 'error', message: 'internal error', error });
+  });
 };
--- routes/rpc.js
+++ routes/rpc.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const controller = require('../controllers/rpc');
 
 router.post('/sendrawtransaction', controller.sendrawtransaction);
+router.get('/getbalance', controller.getbalance);
+router.get('/getunspent', controller.getunspent);
 
 module.exports = router;
