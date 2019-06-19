0fb5555ed74757b0603b6c9eeafdbd318f5c6d83
konez2k <konez@protonmail.com>
2019-06-20 01:46:07 +0200

    remove sessions

--- server.js
+++ server.js
@@ -35,7 +35,6 @@ app.use((req, res, next) => {
 app.use(require('morgan')('dev'));
 app.use(require('body-parser').urlencoded({ limit: '50mb', extended: true }));
 app.use(require('body-parser').json({ limit: '50mb' }));
-app.use(require('express-session')({ secret: config.app.secret, resave: true, saveUninitialized: true }));
 app.use(require('cookie-parser')());
 routeInitialize(app);
 
