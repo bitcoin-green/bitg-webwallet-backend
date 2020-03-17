a03d337d49992a363062abf059f302d00daf8c5b
konez2k <konez@protonmail.com>
2020-03-17 16:10:17 +0100

    remove unused params

--- controllers/rpc.js
+++ controllers/rpc.js
@@ -6,13 +6,11 @@ const client = config.rpcClient;
 
 exports.sendrawtransaction = (req, res) => {
   const raw = req.body.raw;
-  const allowhighfees = stringToBoolean(req.body.allowhighfees);
-  const swifttx = stringToBoolean(req.body.swifttx);
 
   if (!raw) return res.status(400).send({ result: 'error', message: 'Empty raw data!' });
 
   try {
-    return client.call('sendrawtransaction', [raw, allowhighfees, swifttx], (err, result) => {
+    return client.call('sendrawtransaction', [raw], (err, result) => {
       if (err) return res.status(400).send({ result: 'error', message: 'Error occured while sending raw transaction', error: err.error });
       return res.status(200).send({ result: 'ok', data: result });
     });
